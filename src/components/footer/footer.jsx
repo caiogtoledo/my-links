53479f0840b38e617c9bc754b2d01c1b24a26d59
Caio Toledo <caiobbgtoledo@gmail.com>
2025-11-11 20:18:16 -0300

    refat components

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <p className="read-the-docs">
+            Desenvolvido por: Caio Toledo
+        </p>
+    )
+}
+
+export default Footer
---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,18 @@
+import './profile.css'
+
+function Profile() {
+    return (
+        <>
+            <div>
+                <img
+                    src="https://avatars.githubusercontent.com/u/63592244?s=400&u=4838eef867080d58bb9281573b66ee3fce7ab338&v=4"
+                    className="profile-picture"
+                    alt="Foto de perfil"
+                />
+            </div>
+            <h1>Caio Toledo</h1>
+        </>
+    )
+}
+
+export default Profile
